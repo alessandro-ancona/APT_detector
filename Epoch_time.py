ecfa576a3f2486237e3de129956ee4a6f10fec2e
alessandro-ancona <aanconale@outlook.it>
2022-07-10 15:36:11 +0200

    commit 1.5

--- Epoch_time.py
+++ Epoch_time.py
@@ -1,55 +1,61 @@
+# ADDITIONAL PLOTS ----------------------------------------------------
+
 import matplotlib.pyplot as plt
 
+# Epoch duration time vectors for each architecture ratio N_1/N_2
 time_3_1 = [114, 255, 455, 851]
 time_3_2 = [118, 258, 458, 809]
 time_3_3 = [122, 277, 476, 861]
 
+# CE_T vectors for each architecture ratio N_1/N_2
 CE_3_1 = [128.2626, 0.0144, 0.0127, 0.0107]
 CE_3_2 = [110.8878, 0.0144, 0.0121, 0.0104]
 CE_3_3 = [162.4192, 0.0139, 0.0100, 0.0097]
 
+# Accuracy vectors for each architecture ratio N_1/N_2
 A_3_1 = [0.9684, 0.9960, 0.9968, 0.9971]
 A_3_2 = [0.9692, 0.9967, 0.9969, 0.9971]
 A_3_3 = [0.9711, 0.9963, 0.9970, 0.9971]
 
+# LSTM units vector
 LSTM = [0, 64, 128, 256]
 def_x = range(len(LSTM))
 
-# TIME vs L
+# Plot epoch duration vs L for each architecture
 plt.subplots(figsize=(6, 3), dpi=300)
 plt.plot(def_x, time_3_1, '-ob', def_x, time_3_2, '-or', def_x, time_3_3, '-oy', linewidth="0.8")
 plt.ylim([0, 1000])
 plt.xticks(def_x, LSTM)
 plt.xlabel("L", fontsize="medium")
 plt.ylabel("Time [ms]", fontsize="medium")
-plt.legend(('Architectures 3:1', 'Architectures 3:2', 'Architectures 3:3'), fontsize="small", loc='lower right', shadow=True)
+plt.legend(('Architectures 3:1', 'Architectures 3:2', 'Architectures 3:3'),
+           fontsize="small", loc='lower right', shadow=True)
 plt.grid(color='gray', linestyle='--', linewidth=0.2)
 plt.subplots_adjust(left=0.15, right=0.85, bottom=0.15, top=0.85)
 plt.savefig("Epoch_time.eps")
-plt.show()
 
-# Accuracy vs L
+# Plot accuracy vs L for each architecture
 plt.subplots(figsize=(6, 3), dpi=300)
 plt.plot(def_x, A_3_1, '-ob', def_x, A_3_2, '-or', def_x, A_3_3, '-oy', linewidth="0.8")
 plt.ylim([0.9, 1])
 plt.xticks(def_x, LSTM)
 plt.xlabel("L", fontsize="medium")
 plt.ylabel("Accuracy", fontsize="medium")
-plt.legend(('Architectures 3:1', 'Architectures 3:2', 'Architectures 3:3'), fontsize="small", loc='lower right', shadow=True)
+plt.legend(('Architectures 3:1', 'Architectures 3:2', 'Architectures 3:3'),
+           fontsize="small", loc='lower right', shadow=True)
 plt.grid(color='gray', linestyle='--', linewidth=0.2)
 plt.subplots_adjust(left=0.15, right=0.85, bottom=0.15, top=0.85)
 plt.savefig("Accuracy_VS_L.eps")
-plt.show()
 
-# LOSS vs L
+# Plot CE vs L for each architecture
 plt.subplots(figsize=(6, 3), dpi=300)
 plt.plot(def_x, CE_3_1, '-ob', def_x, CE_3_2, '-or', def_x, CE_3_3, '-oy', linewidth="0.8")
 plt.ylim([0, 170])
 plt.xticks(def_x, LSTM)
 plt.xlabel("L", fontsize="medium")
 plt.ylabel("Loss", fontsize="medium")
-plt.legend(('Architectures 3:1', 'Architectures 3:2', 'Architectures 3:3'), fontsize="small", loc='upper right', shadow=True)
+plt.legend(('Architectures 3:1', 'Architectures 3:2', 'Architectures 3:3'),
+           fontsize="small", loc='upper right', shadow=True)
 plt.grid(color='gray', linestyle='--', linewidth=0.2)
 plt.subplots_adjust(left=0.15, right=0.85, bottom=0.15, top=0.85)
 plt.savefig("Loss_VS_L.eps")
-plt.show()
--- multilabel_KDD_unshufflede.py
+++ multilabel_KDD_unshufflede.py
@@ -1,19 +1,23 @@
+# APT DETECTOR ----------------------------------------------------------
+
 import pandas as pd
 import tensorflow as tf
 import numpy as np
-from keras.utils import np_utils
 import random
-from sklearn.preprocessing import LabelEncoder
-import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve
-from sklearn.metrics import confusion_matrix, classification_report
-import seaborn as sns
 import time
+import seaborn as sns
+import matplotlib.pyplot as plt
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, classification_report
+
+# Fix random seeds
 random.seed(123)
 np.random.seed(123)
 tf.random.set_seed(123)
 
 
+# Define callbacks for storing epoch duration
 class TimeHistory(tf.keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
         self.times = []
@@ -25,10 +29,11 @@ class TimeHistory(tf.keras.callbacks.Callback):
         self.times.append(time.time() - self.epoch_time_start)
 
 
+# Data preprocessing
 attack_dict = {
     'normal': 0,
 
-    'back': 4,  # DOS
+    'back': 4,  # DOS -> to remove
     'land': 4,
     'neptune': 4,
     'pod': 4,
@@ -39,14 +44,14 @@ attack_dict = {
     'processtable': 4,
     'udpstorm': 4,
 
-    'ipsweep': 1,  # PROBE
+    'ipsweep': 1,  # PROBE -> IC
     'nmap': 1,
     'portsweep': 1,
     'satan': 1,
     'mscan': 1,
     'saint': 1,
 
-    'ftp_write': 3,  # R2L
+    'ftp_write': 3,  # R2L -> C2
     'guess_passwd': 3,
     'imap': 3,
     'multihop': 3,
@@ -62,7 +67,7 @@ attack_dict = {
     'xsnoop': 3,
     'worm': 3,
 
-    'buffer_overflow': 2,  #U2R
+    'buffer_overflow': 2,  # U2R -> LM
     'loadmodule': 2,
     'perl': 2,
     'rootkit': 2,
@@ -165,159 +170,168 @@ protocol_dict = {
     'udp': 2,
     'icmp': 3
 }
+
+# Import NSL-KDD dataset
 KDDTrain = np.array(pd.read_csv("KDDTrain.csv"))
 KDDTest = np.array(pd.read_csv("KDDTest.csv"))
 
+# Remove row 42 ("Difficulty" is not a feature)
 train_data = np.delete(KDDTrain, 42, 1)
 test_data = np.delete(KDDTest, 42, 1)
-#np.random.shuffle(KDDTrain)
-#np.random.shuffle(KDDTest)
 
-size = train_data.shape[1] - 1
-leng = train_data.shape[0]
+target_feature = train_data.shape[1] - 1
+n_examples = train_data.shape[0]
 
+# Map each categorical feature (as well as the target) to numerical values as show in dictionaries
 for row in train_data:
-    row[size] = attack_dict[row[size]]
+    row[target_feature] = attack_dict[row[target_feature]]
     row[2] = service_dict[row[2]]
     row[1] = protocol_dict[row[1]]
     row[3] = flag_dict[row[3]]
 
 for row in test_data:
-    row[size] = attack_dict[row[size]]
+    row[target_feature] = attack_dict[row[target_feature]]
     row[2] = service_dict[row[2]]
     row[1] = protocol_dict[row[1]]
     row[3] = flag_dict[row[3]]
 
+# Cast data to "float32" for compatibility purposes
 train_data = train_data.astype('float32')
 train_data = train_data.astype('float32')
 test_data = test_data.astype('float32')
 test_data = test_data.astype('float32')
 
-train_data = np.delete(train_data, np.where(train_data[:, size] == 4), axis=0)
-test_data = np.delete(test_data, np.where(test_data[:, size] == 4), axis=0)
+# Remove all DoS examples from either training set and test set
+train_data = np.delete(train_data, np.where(train_data[:, target_feature] == 4), axis=0)
+test_data = np.delete(test_data, np.where(test_data[:, target_feature] == 4), axis=0)
 
-train_examples = train_data[:, :size]
-train_labels = train_data[:, size]
+# Define training examples and the training targets (labels)
+train_examples = train_data[:, :target_feature]
+train_labels = train_data[:, target_feature]
 
-test_examples = test_data[:, :size]
-test_labels = test_data[:, size]
+# Define test examples and the test targets (labels)
+test_examples = test_data[:, :target_feature]
+test_labels = test_data[:, target_feature]
 
+# One-hot-encoding for labels
 encoder = LabelEncoder()
 encoder.fit(train_labels)
 encoded_Y = encoder.transform(train_labels)
 train_categorical_labels = np_utils.to_categorical(encoded_Y)
-
-encoder = LabelEncoder()
 encoder.fit(test_labels)
 encoded_Y = encoder.transform(test_labels)
 test_categorical_labels = np_utils.to_categorical(encoded_Y)
 
+# Converting train and test examples dimensionality for compatibility to LSTM layer
 train_examples = np.expand_dims(train_examples, 1)
 test_examples = np.expand_dims(test_examples, 1)
 
-L = 256  # 64, 128, 256
-neurons1 = 39
-neurons = [13, 26, 39]  # 13 26 39
-file = open("results_performance_" + str(L) +".txt", "w")
+# Define parameters
+alfa = 0.0001  # learning rate
+n_epochs = 100  # number of epochs
+batch_size = 512  # mini-batch size
+val_split = 0.2  # fraction of training data to be used for validation
+n_target_classes = 4  # number of classes to be predicted
+L = 64  # 0, 64, 128, 256
+N_1 = 39
+N_2 = 13  # 13, 26, 39
+file = open("train_performance_" + str(L) + "_" + str(N_1) + "_" + str(N_2) + ".txt", "w")
 
-for neurons2 in neurons:
-    model = tf.keras.Sequential([
-        tf.keras.layers.LSTM(L),
-        tf.keras.layers.Dense(neurons1, activation='relu'),
-        tf.keras.layers.Dense(neurons2, activation='relu'),
-        tf.keras.layers.Dense(4, activation='softmax')])
+# Defining DNN model
+model = tf.keras.Sequential([
+    tf.keras.layers.LSTM(L),
+    tf.keras.layers.Dense(N_1, activation='relu'),
+    tf.keras.layers.Dense(N_2, activation='relu'),
+    tf.keras.layers.Dense(n_target_classes, activation='softmax')])
 
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
-                  loss=tf.keras.losses.CategoricalCrossentropy(),
-                  metrics=[tf.keras.metrics.CategoricalAccuracy(),
-                           tf.keras.metrics.TruePositives(),
-                           tf.keras.metrics.FalsePositives(),
-                           tf.keras.metrics.Precision(),
-                           tf.keras.metrics.Recall(),
-                           tf.keras.metrics.AUC()])
+# Compiling DNN model
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=alfa),
+              loss=tf.keras.losses.CategoricalCrossentropy(),
+              metrics=[tf.keras.metrics.CategoricalAccuracy(),
+                       tf.keras.metrics.Precision(),
+                       tf.keras.metrics.Recall()])
 
-    print("Training for LSTM: " + str(L) + ", neurons1: " + str(neurons1) + ", neurons2: " + str(neurons2))
-    n_epochs = 100
-    time_callback = TimeHistory()
-    history = model.fit(train_examples, train_categorical_labels,
-                        batch_size=512,
-                        epochs=n_epochs,
-                        shuffle=False,
-                        validation_split=0.2,
-                        callbacks=[time_callback])
-    file.write("\nLSTM: " + str(L) + ", " + "NEURONS: " + str(neurons1) + ", " + str(neurons2) + " ***************************************** \n")
-    # print("LSTM: " + str(L) + ", " + "NEURONS: " + str(neurons1) + ", " + str(neurons2) + ": " + str(model.evaluate(test_examples, test_categorical_labels, verbose=False)) + "\n")
+print("Training for architecture -> LSTM: " + str(L) + ", neurons1: " + str(N_1) + ", neurons2: " + str(N_2))
+file.write("ARCHITECTURE -> LSTM = " + str(L) + ", " + "N_1 = " + str(N_1) + ", " + "N_2 = " + str(N_2) + "\n\n")
+time_callback = TimeHistory()
 
-                    # y_pred_keras = model.predict(test_examples).ravel()
-                    # fpr_keras, tpr_keras, threashold_keras = roc_curve(test_labels, y_pred_keras)
+# Fit the model
+history = model.fit(train_examples, train_categorical_labels,
+                    batch_size=batch_size,
+                    epochs=n_epochs,
+                    shuffle=False,
+                    validation_split=val_split,
+                    callbacks=[time_callback])
 
-    # LEARNING CURVES PLOT ---------------------------------------------------------------------------------------------
-
-    plt.subplots(figsize=(6, 3), dpi=300)
-    plt.semilogy(np.arange(n_epochs), history.history['loss'], '-b',
+# Learning Curves plot
+plt.subplots(figsize=(6, 3), dpi=300)
+plt.semilogy(np.arange(n_epochs), history.history['loss'], '-b',
              np.arange(n_epochs), history.history['val_loss'], '-r',
              linewidth="0.8")
-    plt.ylim([10**-3, 10])
-    plt.xlabel("N° Epochs", fontsize="medium")
-    plt.ylabel("Loss", fontsize="medium")
-    plt.legend(('Training Loss', 'Validation Loss'), fontsize="medium", loc='upper right', shadow=True)
-    plt.grid(color='gray', linestyle='--', linewidth=0.2)
-    plt.subplots_adjust(left=0.1, right=0.9, bottom=0.15, top=0.85)
-    plt.savefig("logLC" + str(L) + "_39_" + str(neurons2) + ".eps")
-    #plt.show()
+plt.ylim([10 ** -3, 10])
+plt.xlabel("N° Epochs", fontsize="medium")
+plt.ylabel("Loss", fontsize="medium")
+plt.legend(('Training Loss', 'Validation Loss'), fontsize="medium", loc='upper right', shadow=True)
+plt.grid(color='gray', linestyle='--', linewidth=0.2)
+plt.subplots_adjust(left=0.1, right=0.9, bottom=0.15, top=0.85)
+plt.savefig("logLC_" + str(L) + "_" + str(N_1) + "_" + str(N_2) + ".eps")
 
-    # ACCURACY PLOT ----------------------------------------------------------------------------------------------------
+# Accuracy Plot
+plt.subplots(figsize=(6, 3), dpi=300)
+plt.plot(np.arange(n_epochs), history.history['categorical_accuracy'], '-b',
+         np.arange(n_epochs), history.history['val_categorical_accuracy'], '-r',
+         linewidth="0.8")
+plt.ylim([0, 1])
+plt.xlabel("N° Epochs", fontsize="medium")
+plt.ylabel("Accuracy", fontsize="medium")
+plt.legend(('Training Accuracy', 'Validation Accuracy'), fontsize="medium", loc='lower right', shadow=True)
+plt.grid(color='gray', linestyle='--', linewidth=0.2)
+plt.subplots_adjust(left=0.1, right=0.9, bottom=0.15, top=0.85)
+plt.savefig("ACC_" + str(L) + "_" + str(N_1) + "_" + str(N_2) + ".eps")
 
-    plt.subplots(figsize=(6, 3), dpi=300)
-    plt.plot(np.arange(n_epochs), history.history['categorical_accuracy'], '-b',
-                 np.arange(n_epochs), history.history['val_categorical_accuracy'], '-r',
-                 linewidth="0.8")
-    plt.ylim([0, 1])
-    plt.xlabel("N° Epochs", fontsize="medium")
-    plt.ylabel("Accuracy", fontsize="medium")
-    plt.legend(('Training Accuracy', 'Validation Accuracy'), fontsize="medium", loc='lower right', shadow=True)
-    plt.grid(color='gray', linestyle='--', linewidth=0.2)
-    plt.subplots_adjust(left=0.1, right=0.9, bottom=0.15, top=0.85)
-    plt.savefig("ACC" + str(L) + "_39_" + str(neurons2) + ".eps")
+# Save and print training performance metrics
+print("MEAN EPOCH DURATION: " + str(np.mean(time_callback.times)))
+print("FINAL EPOCH TRAINING ERROR: " + str(history.history['loss'][-1]))
+print("FINAL EPOCH VALIDATION ERROR: " + str(history.history['val_loss'][-1]))
+print("FINAL ACCURACY: " + str(history.history['categorical_accuracy'][-1]) + ", FINAL VALIDATION ACCURACY: "
+      + str(history.history["val_categorical_accuracy"][-1]))
+file.write("MEAN EPOCH DURATION: " + str(np.mean(time_callback.times)) + "\n")
+file.write("FINAL EPOCH TRAINING ERROR: " + str(history.history['loss'][-1]) + "\n")
+file.write("FINAL EPOCH VALIDATION ERROR: " + str(history.history['val_loss'][-1]) + "\n")
+file.write("FINAL ACCURACY: " + str(history.history['categorical_accuracy'][-1]) + ", FINAL VALIDATION ACCURACY: "
+           + str(history.history["val_categorical_accuracy"][-1]) + "\n\n")
 
-    # SAVE AND PRINT PERFORMANCE METRICS -------------------------------------------------------------------------------
+# Evaluate model predictions on test set
+y_pred = model.predict(test_examples)
+y_pred = np.argmax(y_pred, axis=1)
+y_labels = np.argmax(test_categorical_labels, axis=1)
 
-    print("MEAN EPOCH DURATION: " + str(np.mean(time_callback.times)))
-    print("FINAL EPOCH TRAINING ERROR: " + str(history.history['loss'][-1]))
-    print("FINAL EPOCH VALIDATION ERROR: " + str(
-        history.history['val_loss'][-1]))
-    print("FINAL ACCURACY: " + str(history.history['categorical_accuracy'][-1]) + ", FINAL VALIDATION ACCURACY: " + str(history.history["val_categorical_accuracy"][-1]))
-    file.write("MEAN EPOCH DURATION: " + str(np.mean(time_callback.times)) + "\n")
-    file.write("FINAL EPOCH TRAINING ERROR: " + str(history.history['loss'][-1]) + "\n")
-    file.write("FINAL EPOCH VALIDATION ERROR: " + str(history.history['val_loss'][-1]) + "\n")
-    file.write("FINAL ACCURACY: " + str(history.history['categorical_accuracy'][-1]) + ", FINAL VALIDATION ACCURACY: " + str(history.history["val_categorical_accuracy"][-1]) + "\n")
+# Compute classification report on test predictions
+dict_report = classification_report(test_labels, y_pred,
+                                    target_names=['Normal', 'IC', 'LM', 'C2'],
+                                    digits=4,
+                                    zero_division=1,
+                                    output_dict=True)
+text_report = classification_report(test_labels, y_pred,
+                                    target_names=['Normal', 'IC', 'LM', 'C2'],
+                                    digits=4,
+                                    zero_division=1)
+occurrences = [dict_report['Normal']['support'],
+               dict_report['IC']['support'],
+               dict_report['LM']['support'],
+               dict_report['C2']['support']]
 
-    """y_pred = model.predict(test_examples)
-    y_pred = np.argmax(y_pred, axis=1)
-    y_labels = np.argmax(test_categorical_labels, axis=1)
-
-    report = classification_report(test_labels, y_pred,
-                                   target_names=['Normal', 'IC', 'LM', 'C2'],
-                                   digits=4,
-                                   zero_division=1,
-                                   output_dict=True)
-    supports = [report['Normal']['support'],
-                report['IC']['support'],
-                report['LM']['support'],
-                report['C2']['support']]
-    cf_matrix = confusion_matrix(y_labels, y_pred)
-    plt.figure(figsize=(6, 4), dpi=300)
-    ax = sns.heatmap((cf_matrix.T/supports).T, annot=True, cmap='Blues', fmt='.2%') # annot_kws={"fontsize": "large"}
-    ax.set_xlabel('\nPredicted Values', fontsize="xx-large")
-    ax.set_ylabel('Actual Values\n', fontsize="xx-large");
-
-    ## Ticket labels - List must be in alphabetical order
-    ax.xaxis.set_ticklabels(['Normal', 'IC', 'LM', 'C2'])
-    ax.yaxis.set_ticklabels(['Normal', 'IC', 'LM', 'C2'])
-
-    ## Display the visualization of the Confusion Matrix.
-    #plt.show()
-    print(report)
-    #print(model.summary())"""
+# Plot confusion matrix
+cf_matrix = confusion_matrix(y_labels, y_pred)
+plt.subplots(figsize=(6, 3), dpi=300)
+plt.subplots_adjust(left=0.2, right=0.8, bottom=0.2, top=0.8)
+ax = sns.heatmap((cf_matrix.T / occurrences).T, annot=True, cmap='Blues', fmt='.2%')
+ax.set_xlabel('Predicted Values', fontsize="medium")
+ax.set_ylabel('Actual Values', fontsize="medium")
+ax.xaxis.set_ticklabels(['Normal', 'IC', 'LM', 'C2'])
+ax.yaxis.set_ticklabels(['Normal', 'IC', 'LM', 'C2'])
+plt.savefig("CF_" + str(L) + "_" + str(N_1) + "_" + str(N_2) + ".eps")
+file.write(text_report)
+file.write("\n")
 
 file.close()
